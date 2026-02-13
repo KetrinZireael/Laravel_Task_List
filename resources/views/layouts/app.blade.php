ead7da46e1c4d9af08f0600db2b8a9d9293697c1
Kateryna Heletska <geleczkak26@gmail.com>
2026-02-13 21:15:06 +0200

    add errors messages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,10 +2,14 @@
 <html>
 <head>
     <title>Laravel Task List App</title>
+    @yield('styles')
 </head>
 
 <body>
     <h1>@yield('title')</h1>
+    @if(session()->has('success'))
+        <div>{{ session('success') }}</div>
+    @endif
     <div>@yield('content')</div>
 </body>
 
--- routes/web.php
+++ routes/web.php
@@ -1,39 +1,41 @@
 <?php
+
 use App\Models\Task;
 use Illuminate\Http\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return redirect()->route('tasks.index');
-});
+Route::get( '/', function () {
+    return redirect()->route( 'tasks.index' );
+} );
 
-Route::get('/task', function () {
-    return view ('index', [
+Route::get( '/task', function () {
+    return view( 'index', [
         'tasks' => Task::latest()->get()
-    ]);
-})->name('tasks.index');
+    ] );
+} )->name( 'tasks.index' );
 
-Route::view('/tasks/create', 'create');
+Route::view( '/tasks/create', 'create' );
 
-Route::get('/task/{id}', function($id)  {
-    return view ('show',
-            ['task' => Task::findOrFail($id)
-        ]);
-})->name('tasks.show');
+Route::get( '/task/{id}', function ( $id ) {
+    return view( 'show',
+        [
+            'task' => Task::findOrFail( $id )
+        ] );
+} )->name( 'tasks.show' );
 
-Route::post('/tasks', function (Request $request) {
-    $data = request()->validate([
-        'title' => 'required|max:255',
-        'description' => 'required',
+Route::post( '/tasks', function ( Request $request ) {
+    $data = request()->validate( [
+        'title'            => 'required|max:255',
+        'description'      => 'required',
         'long_description' => 'required',
-    ]);
+    ] );
 
-    $task = new Task;
-    $task->title = $data['title'];
-    $task->description = $data['description'];
+    $task                   = new Task;
+    $task->title            = $data['title'];
+    $task->description      = $data['description'];
     $task->long_description = $data['long_description'];
     $task->save();
 
-    return redirect()->route('tasks.show', ['id' => $task->id]);
-})->name('tasks.store');
+    return redirect()->route( 'tasks.show', [ 'id' => $task->id ] )->with('success', 'Task Added');
+} )->name( 'tasks.store' );
