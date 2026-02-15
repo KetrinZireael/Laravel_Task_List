85dce2366ebfbdd595b1430839a25d5f4c9a018a
Kateryna Heletska <geleczkak26@gmail.com>
2026-02-15 21:08:37 +0200

    add tailwind

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,15 +2,16 @@
 <html>
 <head>
     <title>Laravel Task List App</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     @yield('styles')
 </head>
 
-<body>
-    <h1>@yield('title')</h1>
+<body class="container mx-auto mt-10 mb-10 max-w-lg">
+    <h1 class="text-2xl mb-4">@yield('title')</h1>
     @if(session()->has('success'))
         <div>{{ session('success') }}</div>
     @endif
-    <div>@yield('content')</div>
+<div>@yield('content')</div>
 </body>
 
 </html>
